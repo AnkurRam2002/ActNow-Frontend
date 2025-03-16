6b43b81d9b984efd1167ad0f4df651fb0b86fe85
AnkurRam2002 <ankurram2002@gmail.com>
2025-03-16 21:10:52 +0530

    added back to top component

---
+++ src/components/BackToTop.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+import { animateScroll as scroll } from 'react-scroll';
+
+const BackToTop = () => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const toggleVisibility = () => {
+        if (window.scrollY > 200) {
+            setIsVisible(true);
+        } else {
+            setIsVisible(false);
+        }
+    };
+
+    const scrollToTop = () => {
+        scroll.scrollToTop();
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', toggleVisibility);
+        return () => {
+            window.removeEventListener('scroll', toggleVisibility);
+        };
+    }, []);
+
+    return (
+        <div className="fixed bottom-5 right-5 z-1000">
+            {isVisible && 
+                <button onClick={scrollToTop} className="text-white text-2xl py-3.5 px-6 flex items-center justify-center bg-[#463E3E] rounded-full">
+                    ↑
+                </button>
+            }
+        </div>
+    );
+};
+
+export default BackToTop;
