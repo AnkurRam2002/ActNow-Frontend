8f3053f110ea76479f6688da6f369986aad1a435
ayuxi03 <ayushimoitra03@gmail.com>
2025-03-28 01:03:34 +0530

    Added profile toggle menu

--- src/components/HomeHeader.jsx
+++ src/components/HomeHeader.jsx
@@ -1,14 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import profileIcon from '../assets/profileIcon.png'
 import Logo from '../assets/Logo.png'
 import SearchBar from './SearchBar'
+import expandIcon from '../assets/expandIcon.png'
 
 const HomeHeader = () => {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  // Toggle menu visibility
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className='home-header px-[2.5%] py-[1.5%] flex gap-[5%] h-[10%] w-full'>
 
       <div className='left-section w-[20%] flex items-center justify-start gap-[8%]'>
-        <img src={profileIcon} alt="profile" className='h-[60%]' />
+        <img src={profileIcon} alt="profile" className='h-[60%] cursor-pointer' onClick={toggleMenu} />
+
+        {/* Profile menu */}
+        {isMenuOpen && (
+          <div className='profile-menu fixed md:top-[12%] top-[9%] left-[2%] w-[18%] bg-white shadow-[0px_2px_3px_rgba(0,0,0,0.4)] rounded-sm p-[1%] border-1 border-[#e3e1e1] font-[Poppins]'>
+            <p className='text-[1.1vw]'>username</p>
+            <p className='text-[1vw] text-[#6A6767] font-light mb-[5%]'>email123@gmail.com</p>
+            <hr className='border-[#B6B3B3] mb-[4%]' />
+            <div className='flex gap-[4%] items-center'>
+              <p className='text-[#383A42] text-[1.1vw] font-medium'>View profile</p>
+              <img src={expandIcon} alt="expand" className='size-[5%]' />
+            </div>
+            <button className='logout-btn mt-[7%] mx-auto w-full bg-[#FF2C0C] rounded-sm text-white text-[1.2vw] font-medium py-[1.2%] cursor-pointer'>Logout</button>
+          </div>
+        )}
+
         <img src={Logo} alt="logo" className='w-[50%]' />
       </div>
 
