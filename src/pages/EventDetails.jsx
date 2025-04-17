8f1a1d89840332b04caae21eab37f0701815bd47
AnkurRam2002 <ankurram2002@gmail.com>
2025-04-17 10:14:57 +0530

    minor adjustment

--- src/pages/EventDetails.jsx
+++ src/pages/EventDetails.jsx
@@ -51,7 +51,7 @@ const EventDetails = () => {
       }
     } catch (error) {
       console.error("Error participating:", error);
-      alert(error.response?.data?.message || "Failed to participate.");
+      alert(error.response?.data.error || "Failed to participate.");
     }
   };
 
